b896af39e9c63e451d933d08415b58789d33054d
Boyan Liu <53122748+188162600@users.noreply.github.com>
2024-04-16 16:43:03 +0100

    Update transformer_steps_classifier_encoder.py

--- transformer/models/transformer_steps_classifier_encoder.py
+++ transformer/models/transformer_steps_classifier_encoder.py
@@ -463,8 +463,9 @@ class TransformerStepsClassifier(torch.nn.Module):
         logits=logits.view(-1,self.encoder_decoder_layers,self.selective_layers,self.total_options)
         # logits.register_hook(lambda grad: print("classifier grad",grad.sum()))
         logits=zero_lowest_k(logits,self.classifier_cfg.total_options-self.classifier_cfg.options_each_layer ,dim=-1)
+        
+        logits=logits.softmax(dim=-1)
         print("logits",logits)
-        # logits=logits.softmax(dim=-1)
         # epsilon = 1e-5
         # logits=logits/(logits. sum(dim=-1,keepdim=True)+epsilon)
     
